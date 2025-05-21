1a84fa9c0d942dbd698d248720fe694397a8c824
Yash Kumar <Yashxhere@gmail.com>
2025-05-21 15:53:11 +0530

    Refactor script.js to include object structure and user input functionality

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -4,3 +4,17 @@ console.log("Type of x is: ", typeof(x));
 
 let a = "1", b = "1";
 console.log(a - b);
+
+// Objects:-
+let course = {
+    name: "JavaScript",
+    source: "Youtube",
+    price: 999,
+    rating: 4.99
+};
+
+console.log(`Price of the course is: Rs.${course.price}`);
+
+const prompt = require("prompt-sync")();
+let name = prompt("Enter your name: ");
+console.log(`Your name is: ${name}`);
