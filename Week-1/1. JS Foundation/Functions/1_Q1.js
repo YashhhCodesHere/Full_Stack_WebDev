8b9550d8c41ec6468b33aefb101c55c255eafbee
Yash Kumar <Yashxhere@gmail.com>
2025-05-24 10:17:58 +0530

    Add function to calculate the sum of two numbers

---
+++ Week-1/1. JS Foundation/Functions/1_Q1.js
@@ -0,0 +1,10 @@
+// Write a function that finds the sum of two numbers
+
+function sum(num1, num2){
+    return num1+num2;
+}
+
+let num1 = 5;
+let num2 = 8;
+
+console.log(`The sum of ${num1} & ${num2} is: ${sum(num1, num2)}`);
