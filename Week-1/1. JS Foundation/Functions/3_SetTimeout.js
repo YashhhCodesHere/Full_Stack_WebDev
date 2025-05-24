17bd061f90aaf119d3e7c6ba84cecbbfd1250bcc
Yash Kumar <Yashxhere@gmail.com>
2025-05-24 14:48:58 +0530

    Update 3_SetTimeout.js to include setInterval and clearInterval functionality

--- Week-1/1. JS Foundation/Functions/3_SetTimeout.js
+++ Week-1/1. JS Foundation/Functions/3_SetTimeout.js
@@ -5,3 +5,12 @@ function greet(){
 // setTimeout is used to call a function after a particular time/certain duration.
 setTimeout(greet, 5000);    // syntax: setTimeout(function-to-call, time)
 // It's also a callback, as we passed a function as an argument
+
+
+// setInterval is used to call a function repeatedly at a particular interval.
+let id = setInterval(greet, 1000);   // syntax: setInterval(function-to-call, time)
+
+setTimeout(() => {
+    clearInterval(id);
+    console.log("Stopped!");
+}, 5*1000);
